467a00c9118aa15dce5d8693d35f38b37fb524fc
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2023-03-22 17:53:36 +0100

    Combat manager modules created
    Tiene ya las listas de iniciativa pero falta programar que se ordenen de mas a menos

---
+++ Game/Source/Combat.cpp
@@ -0,0 +1,188 @@
+#include "Combat.h"
+
+#include "App.h"
+#include "Audio.h"
+#include "Input.h"
+#include "Physics.h"
+#include "Render.h"
+#include "Textures.h"
+#include "Window.h"
+
+
+#include "IntroScene.h"
+#include "LoseScene.h"
+
+#include "EntityManager.h"
+#include "FadeToBlack.h"
+#include "GuiManager.h"
+#include "Map.h"
+#include "Pathfinding.h"
+
+#include "Defs.h"
+#include "Log.h"
+
+#include <iostream>
+using namespace std;
+#include <sstream>
+
+Combat::Combat() : Module()
+{
+	name.Create("combat");
+}
+
+Combat::~Combat()
+{}
+
+bool Combat::Awake(pugi::xml_node& config)
+{
+	LOG("Loading Scene");
+	bool ret = true;
+
+	return ret;
+}
+
+bool Combat::Start()
+{
+	// Settings
+	pSettings->GUI_id = 0;
+	pSettings->CreateSettings(this);
+
+	// Pause 
+	pPause->GUI_id = pSettings->GUI_id;
+	pPause->CreatePause(this);
+
+	return true;
+}
+
+bool Combat::PreUpdate()
+{
+	return true;
+}
+
+bool Combat::Update(float dt)
+{
+	Debug();
+
+	return true;
+}
+
+bool Combat::PostUpdate()
+{
+	bool ret = true;
+
+	if (exit) return false;
+
+	if(app->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+		ret = false;
+	
+	if (pSettings->settings) { pSettings->OpenSettings(); }
+	if (pPause->pause) { pPause->OpenPause(); }
+	app->guiManager->Draw();
+
+	return ret;
+}
+
+// Called before quitting
+bool Combat::CleanUp()
+{
+	LOG("Freeing scene");
+
+	app->render->camera.x = 0;
+	app->render->camera.y = 0;
+
+	player->Disable();
+
+	app->entityManager->Disable();
+
+	pSettings->CleanUp();
+	pPause->CleanUp();
+	app->guiManager->CleanUp();
+	return true;
+}
+
+void Combat::Debug()
+{
+
+}
+
+bool Combat::InitEntities()
+{
+
+	return true;
+}
+
+bool Combat::OnGuiMouseClickEvent(GuiControl* control)
+{
+	LOG("Event by %d ", control->id);
+
+	app->audio->PlayFx(control->fxControl);
+
+	switch (control->id)
+	{
+	case 1:
+		LOG("Button Close settings click");
+		pPause->OpenPause();
+		pSettings->CloseSettings();
+		break;
+	case 2:
+		LOG("Slider music click");
+		app->audio->ChangeMusicVolume(pSettings->music->volume100);
+		break;
+	case 3:
+		LOG("Slider fx click");
+		app->audio->ChangeFxVolume(pSettings->fx->volume100);
+		break;
+	case 4:
+		LOG("Checkbox Fullscreen click");
+		app->win->changeScreen = !app->win->changeScreen;
+		app->win->ResizeWin();
+		break;
+	case 5:
+		LOG("Checkbox Vsync click");
+		(control->state == GuiControlState::NORMAL) ? app->render->flags = SDL_RENDERER_ACCELERATED : app->render->flags |= SDL_RENDERER_PRESENTVSYNC;
+		break;
+	case 6:
+		LOG("Button Close pause click");
+		pPause->ClosePause(); 
+		break;
+	case 7:
+		LOG("Button Resume click");
+		pPause->ClosePause();
+		break;
+	case 8:
+		LOG("Button Return to Title click");
+		app->fade->FadingToBlack(this, (Module*)app->iScene, 90);
+		break;
+	case 9:
+		LOG("Button settings click");
+		pPause->ClosePause();
+
+		pSettings->settings = !pSettings->settings;
+		if (!pSettings->settings) { pSettings->CloseSettings();}
+		break;
+	case 10:
+		LOG("Button Exit game click");
+		exit = true;
+		break;
+	}
+
+	return true;
+}
+
+bool Combat::AddCombatant(Characther* chara)
+{
+	//We add a Characther to the initiative list and we sort it by speed (fasters first, slowers last)
+	initiative.Add(chara);
+	
+	//initiative.BubbleSort();
+	//if (chara != initiative.start->data) {
+	//	//Bubble Sort Method
+	//	ListItem<Characther*>* aux;
+	//	aux = initiative.end->next;
+	//	//aux = new ListItem<Characther*>;
+	//	while (chara->speed > aux->data->speed)
+	//	{
+	//		//ERIC: Hacer el cambio
+	//	}
+	//}
+}
---
+++ Game/Source/Combat.h
@@ -0,0 +1,75 @@
+#ifndef __COMBAT_H__
+#define __COMBAT_H__
+
+#include "App.h"
+
+#include "Module.h"
+#include "Player.h"
+#include "Characther.h"
+
+#include "GuiButton.h"
+#include "GuiCheckBox.h"
+#include "GuiSliderBar.h"
+
+#include "Settings.h"
+
+struct SDL_Texture;
+
+class Combat : public Module
+{
+public:
+
+	Combat();
+
+	// Destructor
+	virtual ~Combat();
+
+	// Called before render is available
+	bool Awake(pugi::xml_node& config);
+
+	// Called before the first frame
+	bool Start();
+
+	// Called before all Updates
+	bool PreUpdate();
+
+	// Called each loop iteration
+	bool Update(float dt);
+
+	// Called before all Updates
+	bool PostUpdate();
+
+	// Called before quitting
+	bool CleanUp();
+
+	void Debug();
+	bool InitEntities(); //No se si quitarlo o que 
+	
+	bool AddCombatant(Characther* chara); //Eric:Añadir charathers a la lista de personajes
+
+	// Settings
+	bool OnGuiMouseClickEvent(GuiControl* control);
+
+public:
+
+	Player* player;
+	bool frcap;
+
+	//Turn Order
+	List<Characther*> initiative;
+	
+	
+
+private:
+
+	// Settings
+	Settings options;
+	Settings* pSettings = &options;
+
+	Pause pauseMenu;
+	Pause* pPause = &pauseMenu;
+
+	bool exit;
+};
+
+#endif // __COMBAT_H__
