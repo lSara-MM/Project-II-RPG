313ba8ed5886376125a8d18db3b6392573fb0f02
lSara-MM <sqmartinezm@gmail.com>
2023-04-28 12:18:17 +0200

    init enemies rand
    Co-Authored-By: Icefenix7198 <99949593+Icefenix7198@users.noreply.github.com>

--- Game/Source/Combat.cpp
+++ Game/Source/Combat.cpp
@@ -144,24 +144,30 @@ void Combat::Debug()
 
 }
 
-bool Combat::InitCharacters(SString scene)
+bool Combat::InitEnemies(SString scene, vector<int> arr)
 {
-
-	for (pugi::xml_node itemNode = combatNode.child("character"); itemNode; itemNode = itemNode.next_sibling("character"))
+	// to finish
+	for (pugi::xml_node itemNode = combatNode.child("scenes"); itemNode; itemNode = itemNode.next_sibling("character"))
 	{
-		Character* chara = (Character*)app->entityManager->CreateEntity(EntityType::COMBAT_CHARA);
-		chara->parameters = itemNode;
-		chara->Awake();
+		// to test if string compare works
+		if (itemNode.attribute("name").as_string() == scene.GetString())
+		{
+			for (pugi::xml_node itemNode = combatNode.child(scene.GetString()).child("character"); itemNode; itemNode = itemNode.next_sibling("character"))
+			{
+				Character* chara = (Character*)app->entityManager->CreateEntity(EntityType::COMBAT_CHARA);
+				chara->parameters = itemNode;
+				chara->Awake();
 
-		if (chara->charaType == CharacterType::ALLY)
-		{
-			listAllies.push_back(chara);
+				if (chara->charaType == CharacterType::ALLY)
+				{
+					listAllies.push_back(chara);
+				}
+				else if (chara->charaType == CharacterType::ENEMY)
+				{
+					listEnemies.push_back(chara);
+				}
+			}
 		}
-		else if (chara->charaType == CharacterType::ENEMY)
-		{
-			listEnemies.push_back(chara);
-		}
-		
 	}
 	return true;
 }
