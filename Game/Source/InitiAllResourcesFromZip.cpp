71ea965fffb6e217576405798521c3c216e89fec
TheimerTR <albertohidalgo268@gmail.com>
2023-05-14 16:49:18 +0200

    All paths until now added to PhysFS

--- Game/Source/InitiAllResourcesFromZip.cpp
+++ Game/Source/InitiAllResourcesFromZip.cpp
@@ -86,17 +86,94 @@ bool InitAllResourcesFromZip::CleanUp()
 void InitAllResourcesFromZip::LoadTexFile(const pugi::xml_document& dataFile)
 {
 	pugi::xml_node tex = dataFile.child("data").child("texture");
+
+	 texturepathAllyB = tex.attribute("AllyB").as_string();
+	 texturepathAllyP = tex.attribute("AllyP").as_string();
+	 texturepathCH_Prota_Spr = tex.attribute("CH_Prota_Sprites").as_string();
+	 texturepathCH_Prota_Spr2 = tex.attribute("CH_Prota_Sprites2").as_string();
+	 texturepathCheckBox = tex.attribute("CheckBox").as_string();
+	 texturepathCombatBack = tex.attribute("CombatBack").as_string();
+	 texturepathCompa_Spr = tex.attribute("Compa_Sprites").as_string();
+	 texturepathLoseScreen = tex.attribute("LoseScreen").as_string();
+	 texturepathWinScreen = tex.attribute("WinScreen").as_string();
+	 texturepathEnemyCursedPuppet = tex.attribute("EnemyCursedPuppet").as_string();
+	 texturepathFuegito_Chibi = tex.attribute("Fuegito_Chibi").as_string();
+	 texturepathIntro_Bg = tex.attribute("Intro_Bg").as_string();
+	 texturepathLogo_Pikum = tex.attribute("Logo_Pikum").as_string();
+	 texturepathMarioneta2 = tex.attribute("Marioneta2").as_string();
+	 texturepathMarioneta3 = tex.attribute("Marioneta3").as_string();
+	 texturepathOraculo = tex.attribute("Oraculo").as_string();
+	 texturepathProtagonist = tex.attribute("Protagonist").as_string();
+	 texturepathPuzzles = tex.attribute("Puzzles").as_string();
+	 texturepathBoss_Spr = tex.attribute("Boss_Sprite").as_string();
+
 	//player->texture = app->tex->Load(tex.attribute("file2").as_string());
 }
 
 void InitAllResourcesFromZip::LoadAudioFile(const pugi::xml_document& dataFile)
 {
-	pugi::xml_node audio = dataFile.child("data").child("fx");
+	pugi::xml_node audio = dataFile.child("data").child("Audio");
+
+	 texturepathAdivino = audio.attribute("Adivino").as_string();
+	 texturepathButton_Menu = audio.attribute("Button_Menu").as_string();
+	 texturepathChange_Position = audio.attribute("Change_Position").as_string();
+	 texturepathClown_Button = audio.attribute("Clown_Button").as_string();
+	 texturepathConfirm_Interaction = audio.attribute("Confirm_Interaction").as_string();
+	 texturepathEnemy_Selected = audio.attribute("Enemy_Selected").as_string();
+	 texturepathEnemy_Sala = audio.attribute("Enemy_Sala").as_string();
+	 texturepathHeal = audio.attribute("Heal").as_string();
+	 texturepathHit = audio.attribute("Hit").as_string();
+	 texturepathInteraction_Barda = audio.attribute("Interaction_Barda").as_string();
+	 texturepathNegation_Tienda = audio.attribute("Negation_Tienda").as_string();
+	 texturepathOn_Button = audio.attribute("On_Button").as_string();
+	 texturepathPalanca = audio.attribute("Palanca").as_string();
+	 texturepathPasos_Hierba = audio.attribute("Pasos_Hierba").as_string();
+	 texturepathPasos_Roca = audio.attribute("Pasos_Roca").as_string();
+	 texturepathPasos_Solved = audio.attribute("Pasos_Solved").as_string();
+
+	 texturepathCircusTent_Mus = audio.attribute("CircusTent_Mus").as_string();
+	 texturepathHaunted_House = audio.attribute("Haunted_House").as_string();
+	 texturepathIntro_Mus = audio.attribute("Intro_Mus").as_string();
+	 texturepathLobby_Music = audio.attribute("Lobby_Music").as_string();
+	 texturepathLogo_Intro = audio.attribute("Logo_Intro").as_string();
+	 texturepathMus_Combat = audio.attribute("Mus_Combat").as_string();
+	 texturepathMus_Ending = audio.attribute("Mus_Ending").as_string();
+	 texturepathMus_Loose_Scene = audio.attribute("Mus_Loose_Scene").as_string();
+	 texturepathMus_Pause = audio.attribute("Mus_Pause").as_string();
+	 texturepathSala_Entretenimiento = audio.attribute("Sala_Entrenamiento").as_string();
+
 	//app->audio->LoadFx(fx.attribute("file").as_string());
 }
 
 void InitAllResourcesFromZip::LoadGUI(const pugi::xml_document& dataFile)
 {
-	pugi::xml_node GUI = dataFile.child("data").child("mus");
+	pugi::xml_node GUI = dataFile.child("data").child("gui");
+
+	 texturepathUI_Battle_HealthBar = GUI.attribute("UI_Battle_HealthBar").as_string();
+	 texturepathUI_Battle_TurnBar = GUI.attribute("UI_Battle_TurnBar").as_string();
+	 texturepathUI_Button_CharacterSelect = GUI.attribute("UI_Button_CharacterSelect").as_string();
+	 texturepathUI_Buttons = GUI.attribute("UI_Buttons").as_string();
+	 texturepathUI_Diag_Choice_OFF = GUI.attribute("UI_Diag_Choice_OFF").as_string();
+	 texturepathUI_Diag_Ctrl = GUI.attribute("UI_Diag_Ctrl").as_string();
+	 texturepathUI_Diag_Next = GUI.attribute("UI_Diag_Next").as_string();
+	 texturepathUI_E = GUI.attribute("UI_E").as_string();
+	 texturepathUI_Frame_Rect = GUI.attribute("UI_Frame_Rect").as_string();
+	 texturepathUI_Frame_RRect = GUI.attribute("UI_Frame_RRect").as_string();
+	 texturepathUI_Frame_Sqrt = GUI.attribute("UI_Frame_Sqrt").as_string();
+	 texturepathUI_Icons = GUI.attribute("UI_Icons").as_string();
+	 texturepathUI_Marker_L = GUI.attribute("UI_Marker_L").as_string();
+	 texturepathUI_Marker_M = GUI.attribute("UI_Marker_M").as_string();
+	 texturepathUI_Marker_S = GUI.attribute("UI_Marker_S").as_string();
+	 texturepathUI_Slider = GUI.attribute("UI_Slider").as_string();
+	 texturepathUI_Slider2 = GUI.attribute("UI_Slider2").as_string();
+	 texturepathUI_TextBox = GUI.attribute("UI_TextBox").as_string();
+
+	 texturepathButtonInSettings = GUI.attribute("ButtonInSettings").as_string();
+	 texturepathButtonSettings = GUI.attribute("ButtonSettings").as_string();
+	 texturepathClose = GUI.attribute("Close").as_string();
+	 texturepathDialogue = GUI.attribute("Dialogue").as_string();
+	 texturepathGeneric = GUI.attribute("Generic").as_string();
+	 texturepathStart = GUI.attribute("Start").as_string();
+
 	//app->audio->PlayMusic(mus.attribute("file").as_string());
 }
--- Game/Source/InitiAllResourcesFromZip.h
+++ Game/Source/InitiAllResourcesFromZip.h
@@ -50,8 +50,82 @@ public:
 	Player* player;
 	SDL_Texture* img;
 
-private:
+	const char* texturepathAllyB;
+	const char* texturepathAllyP;
+	const char* texturepathCH_Prota_Spr;
+	const char* texturepathCH_Prota_Spr2;
+	const char* texturepathCheckBox;
+	const char* texturepathCombatBack;
+	const char* texturepathCompa_Spr;
+	const char* texturepathLoseScreen;
+	const char* texturepathWinScreen;
+	const char* texturepathEnemyCursedPuppet;
+	const char* texturepathFuegito_Chibi;
+	const char* texturepathIntro_Bg;
+	const char* texturepathLogo_Pikum;
+	const char* texturepathMarioneta2;
+	const char* texturepathMarioneta3;
+	const char* texturepathOraculo;
+	const char* texturepathProtagonist;
+	const char* texturepathPuzzles;
+	const char* texturepathBoss_Spr;
 
+	const char* texturepathAdivino;
+	const char* texturepathButton_Menu;
+	const char* texturepathChange_Position;
+	const char* texturepathClown_Button;
+	const char* texturepathConfirm_Interaction;
+	const char* texturepathEnemy_Selected;
+	const char* texturepathEnemy_Sala;
+	const char* texturepathHeal;
+	const char* texturepathHit;
+	const char* texturepathInteraction_Barda;
+	const char* texturepathNegation_Tienda;
+	const char* texturepathOn_Button;
+	const char* texturepathPalanca;
+	const char* texturepathPasos_Hierba;
+	const char* texturepathPasos_Roca;
+	const char* texturepathPasos_Solved;
+
+	const char* texturepathCircusTent_Mus;
+	const char* texturepathHaunted_House;
+	const char* texturepathIntro_Mus;
+	const char* texturepathLobby_Music;
+	const char* texturepathLogo_Intro;
+	const char* texturepathMus_Combat;
+	const char* texturepathMus_Ending;
+	const char* texturepathMus_Loose_Scene;
+	const char* texturepathMus_Pause;
+	const char* texturepathSala_Entretenimiento;
+
+	const char* texturepathUI_Battle_HealthBar;
+	const char* texturepathUI_Battle_TurnBar;
+	const char* texturepathUI_Button_CharacterSelect;
+	const char* texturepathUI_Buttons;
+	const char* texturepathUI_Diag_Choice_OFF;
+	const char* texturepathUI_Diag_Ctrl;
+	const char* texturepathUI_Diag_Next;
+	const char* texturepathUI_E;
+	const char* texturepathUI_Frame_Rect;
+	const char* texturepathUI_Frame_RRect;
+	const char* texturepathUI_Frame_Sqrt;
+	const char* texturepathUI_Icons;
+	const char* texturepathUI_Marker_L;
+	const char* texturepathUI_Marker_M;
+	const char* texturepathUI_Marker_S;
+	const char* texturepathUI_Slider;
+	const char* texturepathUI_Slider2;
+	const char* texturepathUI_TextBox;
+
+	const char* texturepathButtonInSettings;
+	const char* texturepathButtonSettings;
+	const char* texturepathClose;
+	const char* texturepathDialogue;
+	const char* texturepathGeneric;
+	const char* texturepathStart;
+
+private:
+	
 };
 
 #endif // __LOGOSCENE_H__
