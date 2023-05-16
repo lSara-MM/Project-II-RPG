304fd2d68783e9ede014a446748169cad98101ef
AndyCubico <99950309+AndyCubico@users.noreply.github.com>
2023-05-16 22:23:52 +0200

    Select texture+fix lock movement

--- Game/Source/HouseOfTerrors.cpp
+++ Game/Source/HouseOfTerrors.cpp
@@ -119,12 +119,12 @@ bool HouseOfTerrors::Update(float dt)
 	{
 		if (app->inventory->active)
 		{
-			player->lockMovement = false;
+			//player->lockMovement = false;
 			app->inventory->Disable();
 		}
 		else 
 		{
-			player->lockMovement = true;
+			//player->lockMovement = true;
 			app->inventory->Enable();
 		}
 	}
