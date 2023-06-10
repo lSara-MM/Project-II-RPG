46325fcbff86c4b27243d0a6c63edbca66645c76
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2023-06-10 13:10:12 +0200

    Comentarios

--- Game/Source/Character.h
+++ Game/Source/Character.h
@@ -152,11 +152,11 @@ public:
 	int positionCombat_I;
 	
 	CharacterType charaType; // Ally o Enemy
-	CharacterClass charaClass;
+	CharacterClass charaClass; //Que classe es, esto es para los enemigos
 
 	////Skills Descriptions
-	List<Skill*> listSkills;
-	List<StatusEffect*> listStatusEffects;
+	List<Skill*> listSkills; //Que skills tienen aliados o enemigos (son 4)
+	List<StatusEffect*> listStatusEffects; //Efectos de estado que afectan al chara.
 	List<int> listSkillsHistory; //Aqui guardamos un historial de que skills se ha usado.
 	
 	GuiButton* button;
