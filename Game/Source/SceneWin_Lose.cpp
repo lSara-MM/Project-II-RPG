61fbc3bd493e9501288a3ed24e317a6a5bfafd31
AndyCubico <99950309+AndyCubico@users.noreply.github.com>
2023-06-13 19:44:14 +0200

    Remove tex and fix win

--- Game/Source/SceneWin_Lose.cpp
+++ Game/Source/SceneWin_Lose.cpp
@@ -128,8 +128,7 @@ bool SceneWin_Lose::Update(float dt)
 	{
 		offset = 1300;
 		app->render->DrawTexture(Win, 0, offset + point * (0 - offset));
-		offset = -1300;
-		app->render->TextDraw("VICTORY", 255, offset + point * (50 - offset), 175, Font::TEXT, { 255,255,255 });
+		app->render->TextDraw("VICTORY", 255, - offset + point * (50 + offset), 175, Font::TEXT, { 255,255,255 });
 		if (app->puzzleManager->fightBoss1)
 		{
 			app->puzzleManager->bossIsDead = true;
